1679aeaf16a6288fb342a62a78b605c4fcbdab14
steel-hyuk <skypeed3@naver.com>
2021-08-23 22:37:25 +0900

    inefficiency bracket

---
+++ Level2/rightBracket.js
@@ -0,0 +1,23 @@
+function solution(s){
+    const stack = [];
+    // 열리는 괄호와 닫히는 괄호
+    const open = '(';
+    const close = ')';
+    
+    for (let i=0; i<s.length; i++){
+        // 괄호가 열렸을 때 스택에 담아준다.
+        if (s[i]===open){
+            stack.push(s[i]);
+        }
+        // 닫히는 괄호를 만났을 때 스택에 닫을 짝이 있는지 확인한다
+        else if (s[i] === close) {
+            const check = stack.pop();
+            // 짝이 맞지 않으면 false
+            if (check !== open){
+                return false;
+            }
+        }
+    }
+    // 짝이 다 맞은채로 반복이 끝나더라도 개수가 맞지 않는다면 열린게 더 많음
+    return stack.length === 0;
+}
