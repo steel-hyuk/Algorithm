5e914ba85cb0920690f7ad4a0671da009e593ed4
steel-hyuk <skypeed3@naver.com>
2021-12-09 14:23:46 +0900

    solved 괄호회전하기

--- Level2/괄호회전하기.js
+++ Level2/괄호회전하기.js
@@ -2,39 +2,32 @@ function solution(s) {
   let count = 0;
   // 올바른 괄호 문자열인지 확인하는 함수
   const check = (str) => {
-      // 소,중,대괄호를 확인
-      let sCheck = 0;
-      let mCheck = 0;
-      let lCheck = 0;
+      const stack = [];
       
       for (let i=0; i<str.length; i++){
-          // 괄호에 따라 카운트
-          switch(str[i]) {
-              case '(':
-                  sCheck++;
-                  break;
-              case ')':
-                  sCheck--;
-                  break;
-              case '{':
-                  mCheck++;
-                  break;
-              case '}':
-                  mCheck--;
-                  break;
-              case '[':
-                  lCheck++;
-                  break;
-              case ']':
-                  lCheck--;
-                  break;
+          if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
+              stack.push(str[i]);
+          } else {
+              const open = stack.pop();
+              // 열린 괄호 없이 닫혔을 경우
+              if (open === undefined)
+                  return false;
+              // 괄호가 짝이 맞지 않을 경우
+              switch (str[i]) {
+                  case ')':
+                      if (open !== '(') return false;
+                      break;
+                  case '}':
+                      if (open !== '{') return false;
+                      break;
+                  case ']':
+                      if (open !== '[') return false;
+                      break;
+              }
           }
-          // 닫히는 괄호가 먼저 나온 경우 올바르지 않음
-          if (sCheck < 0 || mCheck < 0 || lCheck < 0)
-              return false;
       }
-      // 짝이 맞지 않은 경우 올바르지 않음
-      if (sCheck !== 0 || mCheck !== 0 || lCheck !== 0)
+      // 열리기만 했을 경우
+      if (stack.length > 0)
           return false;
       return true;
   }
