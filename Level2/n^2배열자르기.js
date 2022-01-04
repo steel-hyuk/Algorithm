b05238e4084191f85a203dcabd8137a64dabc3c0
steel-hyuk <skypeed3@naver.com>
2022-01-04 18:16:08 +0900

    solved n^2 배열 자르기

--- Level2/n^2배열자르기.js
+++ Level2/n^2배열자르기.js
@@ -2,10 +2,10 @@ function solution(n, left, right) {
   const answer = [];
   
   // (i, j)에 위치하는 숫자는 i, j 중 큰 값 + 1
-  for (let i=0; i<n; i++){
-      for (let j=0; j<n; j++){
-          answer.push(Math.max(i, j) + 1);
-      }
+  // 이차원 배열을 다 만들고 자르면 n의 크기가 10의 7승이기 때문에 복잡도가 너무 커진다
+  // 바로 left~right에 맞는 값을 뽑아야 함.
+  for (let i=left; i<=right; i++){
+      answer.push(Math.max(Math.floor(i/n), i % n) + 1);
   }
-  return answer.slice(left, right+1);
+  return answer;
 }
