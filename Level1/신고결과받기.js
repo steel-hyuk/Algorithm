c068aa843fb2af4530c01f805d81b2aacf6fda56
steel-hyuk <skypeed3@naver.com>
2022-01-21 09:40:48 +0900

    solved 신고 결과 받기
    중복을 제거하는 과정에서 배열의 반복을 사용했는데 new Set()을 활용하니
    두 개의 테스트 케이스가 통과하였다.
    이 두 개가 count를 객체가 아닌 배열로 했을 때는 통과했던 테스트 케이스여서
    다시 배열로 바꾸어 통과했다.

--- Level1/신고결과받기.js
+++ Level1/신고결과받기.js
@@ -1,37 +1,31 @@
 function solution(id_list, report, k) {
-  // 중복제거
-  report.sort();
-  for (let i=1; i<report.length; i++){
-      if (report[i-1] === report[i]) {
-          report.splice(i, 1);
-          i--;
-      }
-  }
-  const answer = new Array(id_list.length).fill(0);
-  // report[i][0] => 신고자 / report[i][1] => 불량이용자
-  report = report.map((el) => el.split(' '));
-  // 신고당한 횟수를 기록하는 객체
-  const count = {};
-  for (let i=0; i<report.length; i++){
-      if (Object.keys(count).includes(report[i][1])){
-          count[report[i][1]][0]++;
-          count[report[i][1]].push(report[i][0]);
-      } else {
-          count[report[i][1]] = [1, report[i][0]];
-      }
-  }
-  const callList = [];
-  for (let key in count) {
-      if (count[key][0] >= k) {
-          for (let i=1; i<count[key].length; i++){
-              callList.push(count[key][i]);
-          }
-      }
-  }
-  for (let i=0; i<callList.length; i++){
-      const idx = id_list.indexOf(callList[i]);
-      answer[idx]++;
-  }
-  
-  return answer;
+    const answer = new Array(id_list.length).fill(0);
+    // 중복제거
+    report = [...new Set(report)];
+    // report[i][0] => 신고자 / report[i][1] => 불량이용자
+    report = report.map((el) => el.split(' '));
+    
+    // 신고당한 횟수를 기록하는 배열
+    // [0] 신고당한 사람 [1] 신고한 사람 배열
+    const count = new Array(id_list.length * 2).fill();
+    for (let i=1; i<count.length; i+=2){
+        count[i-1] = id_list[(i-1)/2];
+        count[i] = new Array();
+    }
+    for (let i=0; i<report.length; i++){
+        const idx = count.indexOf(report[i][1]);
+        count[idx+1].push(report[i][0]);
+    }
+    const callList = [];
+    for (let i=1; i<count.length; i+=2) {
+        if (count[i].length >= k) {
+            callList.push(...count[i]);
+        }
+    }
+    for (let i=0; i<callList.length; i++){
+        const idx = id_list.indexOf(callList[i]);
+        answer[idx]++;
+    }
+    
+    return answer;
 }
