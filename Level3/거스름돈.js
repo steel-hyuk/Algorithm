5e97dc2e96c0e0707d70756b23863ca64af64905
steel-hyuk <skypeed3@naver.com>
2022-02-03 11:53:28 +0900

    solved 거스름돈
    dp를 이용

--- Level3/거스름돈.js
+++ Level3/거스름돈.js
@@ -1,3 +1,4 @@
+/*
 function solution(n, money) {
   let check = [];
   // 금액 i를 채울 수 있는 경우의 수 (coins[j]~coins[coins.length-1])
@@ -22,4 +23,17 @@ function solution(n, money) {
     return check[remain][index];
   }
   return func(0, n);
+}
+*/
+function solution(n, money) {
+  const dp = [1, ...new Array(n).fill(0)];
+  
+  for (let i=0; i<money.length; i++) {
+      for (let j=0; j<=n; j++) {
+          if (j >= money[i]) {
+              dp[j] += dp[j-money[i]];
+          }
+      }
+  }
+  return dp[n];
 }
