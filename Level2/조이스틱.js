28f35c0a9928a7ccb86365ad13775806cb14b413
steel-hyuk <skypeed3@naver.com>
2022-02-15 21:31:04 +0900

    unsolved 조이스틱
    A가 연속되는 경우에서 오른쪽으로 이동했다가 하나만을 바꾸고 왼쪽으로 다시 가는 경우가 유리한 케이스가 존재.
    어떻게 경우의 수를 나누어야 할지에 대해 고민이 더 필요하겠음.

--- Level2/조이스틱.js
+++ Level2/조이스틱.js
@@ -1,52 +1,31 @@
 function solution(name) {
-  let answer = 0;
-  let ascii = [];
-  
-  let leftCheckA = 0;
-  let countLCA = 0;
-  let rightCheckA = 0;
-  let countRCA = 0;
-  let move = name.length-1;
-  
-  while(true){
-      rightCheckA++;
-      if (name[rightCheckA] === 'A')
-          countRCA++;
-      else
-          break;
-  }
-  while(true){
-      leftCheckA--;
-      if (leftCheckA === -1){
-          leftCheckA = name.length - 1;
-      }
-      if (name[leftCheckA] === 'A')
-          countLCA++;
-      else
-          break;
-  }
-  
-  if (countLCA < countRCA) { // 왼쪽으로 이동
-      move -= countRCA;
-  }
-  else { // 오른쪽으로 이동
-      move -= countLCA;
-  }
-  answer += move; // 한쪽으로만 이동하는 경우.
-  
-  // ABAAAAAAAABB 로 입력을 받는다면
-      // 오른쪽으로 한칸가서 바꾼 후 왼쪽으로 다시 가는 것이 이득.
-  
-  for (let i=0; i<name.length; i++){
-      ascii[i] = name.charCodeAt(i);
-  }
-  
-  // N : 정방향 13 , 역방향 13
-  // B ~ M 정방향 / O ~ Z 역방향
-      // (Z - name[i] + 1), (name[i] - A) 중 최솟값
-  for (let i=0; i<name.length; i++){
-      answer += Math.min((91 - ascii[i]), (ascii[i] - 65));
-  }
-  
-  return answer;
+    let answer = 0;
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    let target = [];
+    
+    // A가 아닌 알파벳을 변경했을 때의 카운트를 세고 위치를 타겟에 잡아 놓음.
+    for (let i=0; i<name.length; i++){
+        target.push(alphabet.indexOf(name[i]));
+        let changeCnt = (alphabet.indexOf(name[i]) <= 13) ? alphabet.indexOf(name[i]) : 26 - alphabet.indexOf(name[i]);
+        answer += changeCnt;
+    }
+    
+    let move = name.length - 1;
+    
+    for (let i=0; i<target.length; i++){
+        if (target[i] === 0) {
+            let cnt = 1;
+            for (let j=parseInt(i)+1; j<target.length; j++){
+                if (target[j] === 0)
+                    cnt++;
+                else
+                    break;
+            }
+            let leftMove = (i === 0) ? 0 : (parseInt(i) - 1) * 2;
+            let left = leftMove + (target.length - cnt - parseInt(i));
+            if (move > left)
+                move = left;
+        }
+    }
+    return answer + move;
 }
